9a47345f2990ba53d41aaa72fe2801d6f8fc855e
Nikola <nikolastojkovski@yahoo.com>
2021-05-14 22:04:03 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -6,34 +6,20 @@ import Post from "./components/Post";
 import Project from "./components/Project";
 import NavBar from "./components/NavBar";
 
-
 function App() {
   return (
-
     <BrowserRouter>
-    
-    <NavBar />
+      <NavBar />
 
       <Switch>
-
-        
-
-        <Route component={Home} path='/' exact />
-        <Route component={About} path='/about' />
-        <Route component={SinglePost} path='/post/:slug' />
-        <Route component={Post} path='/post' />
-        <Route component={Project} path='/project' />
-
-        
-
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
       </Switch>
-
-     
-
     </BrowserRouter>
-
-  )
-    
+  );
 }
 
 export default App;
